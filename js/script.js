3bad50aeed4fdace50706a0a70ec4264208e42c3
vladflip <vlad.flip.prg@gmail.com>
2014-08-04 14:03:18 +0300

    sdf

--- js/script.js
+++ js/script.js
@@ -1,11 +1,8 @@
 $("#slider").owlCarousel({
-			animateOut: 'fadeOutDown',
 			items:1,
-			margin:400,
 			stagePadding:30,
 			smartSpeed:450,
 			autoplay:true,
 			autoWidth:true,
-			loop:true,
-			margin:150
+			loop:true
 });
